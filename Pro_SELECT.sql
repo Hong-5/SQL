6efdbf3dd69dc4bb21cb649ff709a9dbde485955
Hong-5 <ohongs0731@gmail.com>
2023-01-03 14:54:00 +0900

    2023/01/03

--- Pro_SELECT.sql
+++ Pro_SELECT.sql
@@ -17,3 +17,22 @@ FROM ANIMAL_INS
 ORDER BY ANIMAL_ID;
 
 
+# 2023/01/03
+
+동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 
+이때 결과는 아이디 순으로 조회해주세요.
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID;
+
+
+동물 보호소에 가장 먼저 들어온 동물의 이름을 조회하는 SQL 문을 작성해주세요.
+
+SELECT NAME
+FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1;
+
+
