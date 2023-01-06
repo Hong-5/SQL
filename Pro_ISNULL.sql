655c566093b92efad1c221a548afa3a0d2d766c3
Hong-5 <ohongs0731@gmail.com>
2023-01-06 16:08:15 +0900

    2023/01/06 3 sol

--- Pro_ISNULL.sql
+++ Pro_ISNULL.sql
@@ -1,7 +1,31 @@
 # 2023/01/02
 
+Q. 동물 보호소에 들어온 동물 중, 이름이 있는 동물의 ID를 조회하는 SQL 문을 작성해주세요. 
+단, ID는 오름차순 정렬되어야 합니다.
 
 SELECT ANIMAL_ID
 FROM ANIMAL_INS
 WHERE NAME IS NOT NULL
 ORDER BY ANIMAL_ID;
+
+# 2023/01/06
+
+Q. 입양 게시판에 동물 정보를 게시하려 합니다. 동물의 생물 종, 이름, 성별 및 중성화 여부를 아이디 순으로 조회하는 SQL문을 작성해주세요. 
+이때 프로그래밍을 모르는 사람들은 NULL이라는 기호를 모르기 때문에, 이름이 없는 동물의 이름은 "No name"으로 표시해 주세요.
+
+SELECT ANIMAL_TYPE,
+    CASE
+        WHEN NAME IS NULL THEN "No name" 
+        ELSE NAME
+    END as NAME,
+    SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
+
+Q. USER_INFO 테이블에서 나이 정보가 없는 회원이 몇 명인지 출력하는 SQL문을 작성해주세요. 
+이때 컬럼명은 USERS로 지정해주세요.
+
+SELECT COUNT(USER_ID) AS USERS
+FROM USER_INFO
+WHERE AGE IS NULL;
+
--- Pro_SELECT.sql
+++ Pro_SELECT.sql
@@ -1,7 +1,7 @@
 # 2023/01/02
 
 
-동물 보호소에 들어온 모든 동물의 아이디와 이름, 보호 시작일을 이름 순으로 조회하는 SQL문을 작성해주세요.
+Q. 동물 보호소에 들어온 모든 동물의 아이디와 이름, 보호 시작일을 이름 순으로 조회하는 SQL문을 작성해주세요.
 단, 이름이 같은 동물 중에서는 보호를 나중에 시작한 동물을 먼저 보여줘야 합니다.
 
 SELECT ANIMAL_ID, NAME, DATETIME
@@ -10,7 +10,7 @@ ORDER BY NAME, DATETIME DESC;
 
 
 
-동물 보호소에 들어온 모든 동물의 아이디와 이름을 ANIMAL_ID순으로 조회하는 SQL문을 작성해주세요. 
+Q. 동물 보호소에 들어온 모든 동물의 아이디와 이름을 ANIMAL_ID순으로 조회하는 SQL문을 작성해주세요. 
 
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
@@ -19,7 +19,7 @@ ORDER BY ANIMAL_ID;
 
 # 2023/01/03
 
-동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 
+Q. 동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 
 이때 결과는 아이디 순으로 조회해주세요.
 
 SELECT ANIMAL_ID, NAME
@@ -28,7 +28,7 @@ WHERE INTAKE_CONDITION = 'Sick'
 ORDER BY ANIMAL_ID;
 
 
-동물 보호소에 가장 먼저 들어온 동물의 이름을 조회하는 SQL 문을 작성해주세요.
+Q. 동물 보호소에 가장 먼저 들어온 동물의 이름을 조회하는 SQL 문을 작성해주세요.
 
 SELECT NAME
 FROM ANIMAL_INS
@@ -38,6 +38,9 @@ LIMIT 1;
 
 # 2023/01/04
 
+Q. 동물 보호소에 들어온 동물 중 젊은 동물의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 
+이때 결과는 아이디 순으로 조회해주세요.
+
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
 WHERE INTAKE_CONDITION != "Aged"
@@ -46,6 +49,20 @@ ORDER BY ANIMAL_ID;
 
 # 2023/01/05
 
+Q. 동물 보호소에 들어온 모든 동물의 이름과 보호 시작일을 조회하는 SQL문을 작성해주세요. 
+이때 결과는 ANIMAL_ID 역순으로 보여주세요. SQL을 실행하면 다음과 같이 출력되어야 합니다.
+
 SELECT NAME, DATETIME
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID DESC;
+
+
+#2023/01/06
+
+Q. FOOD_FACTORY 테이블에서 강원도에 위치한 식품공장의 공장 ID, 공장 이름, 주소를 조회하는 SQL문을 작성해주세요. 
+이때 결과는 공장 ID를 기준으로 오름차순 정렬해주세요.
+
+SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
+FROM FOOD_FACTORY
+WHERE ADDRESS LIKE "강원도%"
+ORDER BY FACTORY_ID;
