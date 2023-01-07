1749d476f25229c9010e9e89123068331497c137
Hong-5 <ohongs0731@gmail.com>
2023-01-07 23:28:09 +0900

    2023/01/07 3sol

--- Pro_GROUPBY.sql
+++ Pro_GROUPBY.sql
@@ -5,3 +5,14 @@ FROM ANIMAL_INS
 GROUP BY NAME
 HAVING NAME IS NOT NULL AND COUNT >= 2
 ORDER BY NAME;
+
+
+# 2023/01/07
+
+동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문을 작성해주세요. 
+이때 고양이를 개보다 먼저 조회해주세요.
+
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_ID) as count
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE;
