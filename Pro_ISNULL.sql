5ad8622cc9eb08e23470842f14946b0fe8bb8033
Hong-5 <ohongs0731@gmail.com>
2023-01-08 16:38:15 +0900

    2023/01/08 : 3sol

--- Pro_ISNULL.sql
+++ Pro_ISNULL.sql
@@ -29,3 +29,25 @@ SELECT COUNT(USER_ID) AS USERS
 FROM USER_INFO
 WHERE AGE IS NULL;
 
+
+# 2023/01/08
+
+Q. 동물 보호소에 들어온 동물 중, 이름이 없는 채로 들어온 동물의 ID를 조회하는 SQL 문을 작성해주세요. 
+단, ID는 오름차순 정렬되어야 합니다.
+
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NULL
+ORDER BY ANIMAL_ID;
+
+Q. FOOD_WAREHOUSE 테이블에서 경기도에 위치한 창고의 ID, 이름, 주소, 냉동시설 여부를 조회하는 SQL문을 작성해주세요. 
+이때 냉동시설 여부가 NULL인 경우, 'N'으로 출력시켜 주시고 결과는 창고 ID를 기준으로 오름차순 정렬해주세요.
+
+SELECT WAREHOUSE_ID, WAREHOUSE_NAME, ADDRESS,
+    CASE
+        WHEN FREEZER_YN IS NULL THEN 'N'
+        ELSE FREEZER_YN
+    END
+FROM FOOD_WAREHOUSE
+WHERE ADDRESS LIKE "경기도 %"
+ORDER BY WAREHOUSE_ID;
--- Pro_SUM_MAX_MIN.sql
+++ Pro_SUM_MAX_MIN.sql
@@ -1,25 +1,33 @@
 # 2023/01/03
 
-동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문을 작성해주세요.
+Q. 동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문을 작성해주세요.
 
 SELECT COUNT(ANIMAL_ID) AS count
 FROM ANIMAL_INS;
 
 
-
 # 2023/01/04
 
-동물 보호소에 들어온 동물의 이름은 몇 개인지 조회하는 SQL 문을 작성해주세요. 
+Q. 동물 보호소에 들어온 동물의 이름은 몇 개인지 조회하는 SQL 문을 작성해주세요. 
 이때 이름이 NULL인 경우는 집계하지 않으며 중복되는 이름은 하나로 칩니다.
 
 SELECT COUNT(DISTINCT(NAME)) AS count
 FROM ANIMAL_INS
 WHERE NAME IS NOT NULL;
 
-
-동물 보호소에 가장 먼저 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+Q. 동물 보호소에 가장 먼저 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
 
 SELECT DATETIME AS 시간
 FROM ANIMAL_INS
 ORDER BY DATETIME
+LIMIT 1;
+
+
+# 2023/01/08
+
+Q. FOOD_PRODUCT 테이블에서 가격이 제일 비싼 식품의 식품 ID, 식품 이름, 식품 코드, 식품분류, 식품 가격을 조회하는 SQL문을 작성해주세요.
+
+SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE
+FROM FOOD_PRODUCT
+ORDER BY PRICE DESC
 LIMIT 1;
