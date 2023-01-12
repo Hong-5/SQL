9385b9d8d3a51aa7d107ff06038b682963e40592
Hong-5 <ohongs0731@gmail.com>
2023-01-12 12:12:18 +0900

    2023/01/12 : 3sol

--- Pro_SELECT.sql
+++ Pro_SELECT.sql
@@ -76,3 +76,35 @@ SELECT COUNT(USER_ID) AS USERS
 FROM USER_INFO
 WHERE (JOINED LIKE "2021%") AND (AGE BETWEEN 20 AND 29);
 
+
+# 2023/01/12
+
+Q. DOCTOR 테이블에서 진료과가 흉부외과(CS)이거나 일반외과(GS)인 의사의 이름, 의사ID, 진료과, 고용일자를 조회하는 SQL문을 작성해주세요. 
+이때 결과는 고용일자를 기준으로 내림차순 정렬하고, 고용일자가 같다면 이름을 기준으로 오름차순 정렬해주세요.
+
+SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, "%Y-%m-%d") AS HIRE_YMD
+FROM DOCTOR
+WHERE MCDP_CD IN ("CS", "GS")
+ORDER BY HIRE_YMD DESC, DR_NAME;
+
+
+Q. PATIENT 테이블에서 12세 이하인 여자환자의 환자이름, 환자번호, 성별코드, 나이, 전화번호를 조회하는 SQL문을 작성해주세요. 
+이때 전화번호가 없는 경우, 'NONE'으로 출력시켜 주시고 결과는 나이를 기준으로 내림차순 정렬하고, 나이 같다면 환자이름을 기준으로 오름차순 정렬해주세요.
+
+SELECT PT_NAME, PT_NO, GEND_CD, AGE, 
+    (CASE
+        WHEN TLNO IS NULL THEN "NONE"
+        ELSE TLNO
+    END) AS TLNO
+FROM PATIENT
+WHERE (AGE <= 12) AND (GEND_CD = "W")
+ORDER BY AGE DESC, PT_NAME;
+
+
+Q. MEMBER_PROFILE 테이블에서 생일이 3월인 여성 회원의 ID, 이름, 성별, 생년월일을 조회하는 SQL문을 작성해주세요. 
+이때 전화번호가 NULL인 경우는 출력대상에서 제외시켜 주시고, 결과는 회원ID를 기준으로 오름차순 정렬해주세요.
+
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') AS DATE_OF_BIRTH
+FROM MEMBER_PROFILE
+WHERE (TLNO IS NOT NULL) AND (GENDER = 'W') AND (DATE_FORMAT(DATE_OF_BIRTH, '%m') = "03")
+ORDER BY MEMBER_ID;
