e066e8f8d269a3e2c2c9989f91b5be838a024e95
Hong-5 <ohongs0731@gmail.com>
2023-01-13 19:02:52 +0900

    2023/01/13 : 3sol

--- Pro_JOIN.sql
+++ Pro_JOIN.sql
@@ -34,3 +34,16 @@ FROM ANIMAL_INS
 JOIN ANIMAL_OUTS ON ANIMAL_INS.ANIMAL_ID = ANIMAL_OUTS.ANIMAL_ID
 WHERE ANIMAL_INS.DATETIME > ANIMAL_OUTS.DATETIME
 ORDER BY ANIMAL_INS.DATETIME;
+
+
+# 2023/01/13
+
+
+Q. PRODUCT 테이블과 OFFLINE_SALE 테이블에서 상품코드 별 매출액(판매가 * 판매량) 합계를 출력하는 SQL문을 작성해주세요.
+결과는 매출액을 기준으로 내림차순 정렬해주시고 매출액이 같다면 상품코드를 기준으로 오름차순 정렬해주세요.
+
+SELECT product_code, SUM(sales_amount * price) AS SALES
+FROM OFFLINE_SALE
+JOIN PRODUCT ON OFFLINE_SALE.PRODUCT_ID = PRODUCT.PRODUCT_ID
+GROUP BY product_code
+ORDER BY SALES DESC, product_code;
