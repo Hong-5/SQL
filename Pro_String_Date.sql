aeb4f5264bdbace3a0c3435c856ed118792f86c2
Hong-5 <ohongs0731@gmail.com>
2023-01-22 21:49:08 +0900

    2023/01/22 : 1sol

--- Pro_String_Date.sql
+++ Pro_String_Date.sql
@@ -87,3 +87,18 @@ JOIN DOCTOR ON DOCTOR.DR_ID = APPOINTMENT.MDDR_ID
 WHERE DOCTOR.MCDP_CD = 'CS' AND DATE_FORMAT(APPOINTMENT.APNT_YMD, "%Y-%m-%d") = "2022-04-13" AND APPOINTMENT.APNT_CNCL_YN = 'N') AS tmp
 JOIN PATIENT ON PATIENT.PT_NO = tmp.PT_NO
 ORDER BY APNT_YMD;
+
+
+# 2023/01/22
+
+Q. CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 대여 시작일이 2022년 9월에 속하는 대여 기록에 대해서 대여 기간이 30일 이상이면 '장기 대여' 그렇지 않으면 '단기 대여' 로 표시하는 컬럼(컬럼명: RENT_TYPE)을 추가하여 대여기록을 출력하는 SQL문을 작성해주세요.
+결과는 대여 기록 ID를 기준으로 내림차순 정렬해주세요.
+
+SELECT history_id, car_id , DATE_FORMAT(start_date, "%Y-%m-%d") AS START_DATE, 
+    DATE_FORMAT(end_date, "%Y-%m-%d") AS END_DATE, 
+    (CASE WHEN DATEDIFF(END_DATE, START_DATE) >= 29 THEN "장기 대여"
+        ELSE "단기 대여"
+    END ) AS RENT_TYPE
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE START_DATE LIKE "2022-09-%" 
+ORDER BY history_id DESC;
