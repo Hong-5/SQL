ae540f02f49e84ae6bc656246e5dedf68a1d4b47
Hong-5 <ohongs0731@gmail.com>
2023-01-25 15:10:10 +0900

    2023/01/25 : 3sol

--- Pro_GROUPBY.sql
+++ Pro_GROUPBY.sql
@@ -154,4 +154,23 @@ FROM
     JOIN BOOK AS B ON A.book_id = B.book_id) AS T
 JOIN AUTHOR ON T.AUTHOR_ID = AUTHOR.AUTHOR_ID
 GROUP BY AUTHOR_NAME,CATEGORY
-ORDER BY AUTHOR_ID ASC, CATEGORY DESC
+ORDER BY AUTHOR_ID ASC, CATEGORY DESC;
+
+
+
+# 2023/01/25
+
+Q. CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 대여 시작일을 기준으로 2022년 8월부터 2022년 10월까지 총 대여 횟수가 5회 이상인 자동차들에 대해서 해당 기간 동안의 월별 자동차 ID 별 총 대여 횟수(컬럼명: RECORDS) 리스트를 출력하는 SQL문을 작성해주세요. 
+결과는 월을 기준으로 오름차순 정렬하고, 월이 같다면 자동차 ID를 기준으로 내림차순 정렬해주세요. 특정 월의 총 대여 횟수가 0인 경우에는 결과에서 제외해주세요.
+
+SELECT MONTH(START_DATE) AS MONTH, CAR_ID, COUNT(HISTORY_ID) AS RECORDS
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE CAR_ID IN
+    (SELECT car_id
+        FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+        WHERE MONTH(start_date) IN (8,9,10)
+        GROUP BY car_id
+        HAVING COUNT(history_id) >= 5)# 대여 횟수가 5회 이상인 자동차
+        AND MONTH(START_DATE) IN (8,9,10)
+GROUP BY MONTH(START_DATE), CAR_ID
+ORDER BY MONTH, CAR_ID DESC;
