f9781cd8d8334ea6697f57aa03a90a42635d4a56
Hong-5 <ohongs0731@gmail.com>
2023-01-31 12:47:12 +0900

    2023/01/31 : 3sol

--- HR_Basic_Select.sql
+++ HR_Basic_Select.sql
@@ -122,3 +122,26 @@ FROM STATION
 WHERE LEFT(CITY, 1) NOT IN ("a", 'e', 'i', 'o', 'u') 
     and RIGHT(CITY, 1) NOT IN ("a", 'e', 'i', 'o', 'u')
 
+# 2023/01/31
+
+Q. Query the Name of any student in STUDENTS who scored higher than 75  Marks.Order your output by the last three characters of each name. 
+If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+SELECT NAME
+FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY RIGHT(NAME,3), ID
+
+Q. Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+SELECT name
+FROM Employee
+ORDER BY name
+
+Q. Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than  per month who have been employees for less than  months. 
+Sort your result by ascending employee_id.
+
+SELECT NAME
+FROM Employee
+WHERE MONTHS < 10 AND SALARY > 2000
+ORDER BY employee_id;
